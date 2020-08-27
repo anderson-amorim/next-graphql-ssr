5442041ba8a33687869428fc99d39a4bcfdab80e
anderson-amorim <anderson.amorim@heathmont.net>
2020-08-27 12:59:08 +0300

    enabling rewrites

--- next.config.js
+++ next.config.js
@@ -8,24 +8,24 @@ const config = {
   //     { source: "/en/:path*", destination: "/:path*", permanent: true },
   //   ];
   // },
-  // async rewrites() {
-  //   return [
-  //     { source: "/ca/:path*", destination: "/ca/:path*" },
-  //     { source: "/de/:path*", destination: "/de/:path*" },
-  //     { source: "/ee/:path*", destination: "/ee/:path*" },
-  //     { source: "/es/:path*", destination: "/es/:path*" },
-  //     { source: "/ja/:path*", destination: "/ja/:path*" },
-  //     { source: "/ko/:path*", destination: "/ko/:path*" },
-  //     { source: "/fi/:path*", destination: "/fi/:path*" },
-  //     { source: "/pt/:path*", destination: "/pt/:path*" },
-  //     { source: "/ru/:path*", destination: "/ru/:path*" },
-  //     { source: "/th/:path*", destination: "/th/:path*" },
-  //     { source: "/tr/:path*", destination: "/tr/:path*" },
-  //     { source: "/zh/:path*", destination: "/zh/:path*" },
-  //     { source: "/:path*", destination: "/en/:path*" },
-  //     { source: "/", destination: "/en" },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      { source: "/ca/:path*", destination: "/ca/:path*" },
+      { source: "/de/:path*", destination: "/de/:path*" },
+      { source: "/ee/:path*", destination: "/ee/:path*" },
+      { source: "/es/:path*", destination: "/es/:path*" },
+      { source: "/ja/:path*", destination: "/ja/:path*" },
+      { source: "/ko/:path*", destination: "/ko/:path*" },
+      { source: "/fi/:path*", destination: "/fi/:path*" },
+      { source: "/pt/:path*", destination: "/pt/:path*" },
+      { source: "/ru/:path*", destination: "/ru/:path*" },
+      { source: "/th/:path*", destination: "/th/:path*" },
+      { source: "/tr/:path*", destination: "/tr/:path*" },
+      { source: "/zh/:path*", destination: "/zh/:path*" },
+      { source: "/:path*", destination: "/en/:path*" },
+      { source: "/", destination: "/en" },
+    ];
+  },
 };
 
 module.exports = config;
