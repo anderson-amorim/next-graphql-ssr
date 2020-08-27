b2bafb23aea85a2316f764e149489f6e02f2d3e1
anderson-amorim <anderson.amorim@heathmont.net>
2020-08-27 11:35:54 +0300

    removing matifest meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,20 +30,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-          />
-          <meta name="description" content="Description" />
-          <meta name="keywords" content="Keywords" />
-          <title>Next.js PWA Example</title>
-
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#317EFB" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
